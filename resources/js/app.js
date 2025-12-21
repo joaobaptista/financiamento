608ab7a6cc7452e22dd1cc4fbbd6c3e0bdecf18f
Vagner Ribas <vameri@gmail.com>
2025-12-20 23:23:08 -0300

    Add Vue 3 frontend setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { createApp } from 'vue';
+import App from './vue/App.vue';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const vueRoot = document.getElementById('vue-app');
+
+if (vueRoot) {
+	createApp(App).mount(vueRoot);
+}
